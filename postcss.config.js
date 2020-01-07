a0351b653537c6b08f2a852edeb405dacff08049
Artem Valikov <doksna@mail.ru>
2020-01-07 20:43:59 +0300

    Webpack install Pug

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,9 @@
+// autoprefixer - https://github.com/postcss/autoprefixer
+// css-mqpacker - https://github.com/hail2u/node-css-mqpacker
+// cssnano      - https://github.com/hail2u/node-css-mqpacker
+
 // npm install postcss-loader autoprefixer css-mqpacker cssnano --save-dev
+
 module.exports = {
   plugins: [
     require('autoprefixer'),
@@ -7,7 +12,7 @@ module.exports = {
       preset: [
         'default', {
           discardComments: {
-            removeAll: true,
+            removeAll: true
           }
         }
       ]
--- src/index.js
+++ src/index.js
@@ -1,36 +1,19 @@
-// JS - ./js/index.js
-import './js/common'
+// JS
+import './js/'
+
 // SCSS
-import './scss/main.scss'
+import './assets/scss/main.scss'
+
 // CSS (example)
-import './css/main.css'
-
-// Bootstrap (example)
-// import Bootstrap from 'bootstrap/dist/css/bootstrap.min.css'
-// import 'bootstrap/dist/css/bootstrap.min.css'
-
-// import 'vue'
-// import Vue from 'vue'
+// import './assets/css/main.css'
 
 // Vue.js
 window.Vue = require('vue')
-import store from './store'
 
 // Vue components (for use in html)
 Vue.component('example-component', require('./components/Example.vue').default)
 
 // Vue init
 const app = new Vue({
-  data () {
-    return {
-      showExampleComponent: false,
-    }
-  },
-  store,
   el: '#app'
 })
-
-// Without vuex and store
-// const app = new Vue({
-//   el: '#app'
-// }) 

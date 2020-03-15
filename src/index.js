37ff49c990733f932754ccc633f395b9b7704c61
Artem Valikov <doksna@mail.ru>
2020-03-15 15:42:56 +0300

    Подключение библиотеки на примере Vue

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './js/common'
+
+// import 'vue'
+
+// import 'bootstrap/dist/css/bootstrap.min.css' //если мы хотим подключить не всю библиотеку, а например только стили
+
+// import Bootstrap from 'bootstrap/dist/css/bootstrap.min.css' //-||- + можем взаимодействовать
+
+// import Vue from 'vue' //- если мы хотим с ней взаимодействовать
+// Vue.use()
+
+window.Vue = require('vue')

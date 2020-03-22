97cff21a38c1969d5c9051e7cc9d6caa9a998c34
Artem Valikov <doksna@mail.ru>
2020-03-22 21:58:36 +0300

    Настройка препроцессора SASS. Создание scss и css папок в webpack; Установка mini-css-extract-plugin; Webpack 4 SASS. Обработка;

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
 module.exports = {
+
 	entry: {
 		app: './src/index.js'
 	},
@@ -10,15 +13,42 @@ module.exports = {
 		publicPath: '/dist'
 	},
 
-	devServer: {
-		overlay: true
-	},
-
 	module: {
 		rules: [{
 			test: /\.js$/,
 			loader: 'babel-loader',
 			exclude: '/node_modules/'
-		}]
-	}
+		}, {
+			test: /\.css$/,
+			use: [
+				MiniCssExtractPlugin.loader,
+				"css-loader"
+			]
+		}, {
+			test: /\.scss$/,
+			use: [
+				'style-loader',
+				MiniCssExtractPlugin.loader,
+				{
+					loader: 'css-loader',
+					options: { sourceMap: true }
+				}, {
+					loader: 'sass-loader',
+					options: { sourceMap: true }
+				}
+			]
+		}
+		]
+	},
+
+	devServer: {
+		overlay: true
+	},
+
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: "[name].css"
+		})
+	],
+
 }

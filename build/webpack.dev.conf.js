24b6d3d83b1c2bfeb9d3947377568cd5f1a45f68
Artem Valikov <doksna@mail.ru>
2020-03-23 11:19:29 +0300

    Webpack devtool - карта для нашего CSS

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -5,16 +5,24 @@ const baseWebpackConfig = require('./webpack.base.conf')
 const devWebpackConfig = merge(baseWebpackConfig, {
   // DEV settings gonna be here
   mode: 'development',
+  devtool: 'cheap-module-eval-source-map',
 
   devServer: {
     // historyApiFallback: true,
     // noInfo: true,
+    port: 8081,
     overlay: {
       warnings: true,
       errors: true
-    },
-    port: 8081,
+    }
   },
+
+  plugins: [
+    new webpack.SourceMapDevToolPlugin({
+      filename: '[file].map'
+    })
+  ]
+
 })
 
 // export devWebpackConfig

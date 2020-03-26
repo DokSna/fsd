60bcef9dd371c84842d158d5e8e5492720cade47
Artem Valikov <doksna@mail.ru>
2020-03-26 17:01:12 +0300

    Начата вёрстка страницы "Colors & Type". Установлены: favicon.ico, title, Логотип в header. Добавлены папки blocks для scss и pug файлов.

--- src/index.js
+++ src/index.js
@@ -5,32 +5,33 @@ import './assets/scss/main.scss'
 // import Bootstrap from 'bootstrap/dist/css/bootstrap.min.css'
 // import 'bootstrap/dist/css/bootstrap.min.css'
 
-// import 'vue'
-// import Vue from 'vue'
-window.Vue = require('vue') //сдесь мы Vue подвешиваем на window, =~
+// // import 'vue'
+// // import Vue from 'vue'
+// window.Vue = require('vue') //сдесь мы Vue подвешиваем на window, =~
 
-// Vue components (for use in html)
-Vue.component('example-component', require('./components/Example.vue').default)	//~=а далее мы уже к ней обращаемся.
+// // Vue components (for use in html)
+// Vue.component('example-component', require('./components/Example.vue').default)	//~=а далее мы уже к ней обращаемся.
 
-import store from './store'
+// import store from './store'
 
-// With vuex
-const app = new Vue({
-  data () {
-    return {
-      component: false, //что бы по умолчанию выкл была
-    }
-  },
-  store,
-  el: '#app'
-})
-
-// // Vue init
+// // With vuex
 // const app = new Vue({
+//   data () {
+//     return {
+//       component: false, //что бы по умолчанию выкл была
+//     }
+//   },
+//   store,
 //   el: '#app'
 // })
 
-//* Without vuex and store
-// const app = new Vue({
-//   el: '#app'
-// })
+// // // Vue init
+// // const app = new Vue({
+// //   el: '#app'
+// // })
+
+// //* Without vuex and store
+// // const app = new Vue({
+// //   el: '#app'
+// // })
+//Закомментировал Vue пока его не использую

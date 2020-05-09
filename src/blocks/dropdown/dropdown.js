93312ae3e2403a2f0e3edb1a8ce4089ec6492ac4
Artem Valikov <doksna@mail.ru>
2020-05-09 22:26:19 +0300

    В дропдауне выводит сумму гостей

---
+++ src/blocks/dropdown/dropdown.js
@@ -0,0 +1,29 @@
+// let dropDowns = document.querySelectorAll(".dropdown");
+// for (i = 0; i < dropDowns.length; i++) {
+//   calcItems = dropDowns[i].querySelectorAll(".calc-item__value");
+//   let calcItemsSum = 0;
+//   for (j = 0; j < calcItems.length; j++) {
+//     calcItemsSum += Number(calcItems[j].value);
+//   }
+//   let info = dropDowns[i].querySelector(".dropdown__info > input");
+//   info.value = calcItemsSum;
+// }
+function dropDownsys() {
+  let dropDowns = document.querySelectorAll(".dropdown");
+  for (i = 0; i < dropDowns.length; i++) {
+    calcItems = dropDowns[i].querySelectorAll(".calc-item__value");
+    let calcItemsSum = 0;
+    for (j = 0; j < calcItems.length; j++) {
+      calcItemsSum += Number(calcItems[j].value);
+    }
+    let info = dropDowns[i].querySelector(".dropdown__info > input");
+    info.value = calcItemsSum;
+  }
+}
+
+document.addEventListener('DOMContentLoaded', dropDownsys())
+
+// let calcClick = document.querySelectorAll(".calc-item__operator");
+// for (i = 0; i < calcClick.length; i++) {
+//   calcClick[i].addEventListener("click", dropDownsys());
+// }

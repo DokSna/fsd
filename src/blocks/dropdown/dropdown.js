297ec057480ef0c3efbeee5782b579131e68f495
Artem Valikov <doksna@mail.ru>
2020-05-12 17:16:35 +0300

    Dropdown is full ready!

--- src/blocks/dropdown/dropdown.js
+++ src/blocks/dropdown/dropdown.js
@@ -1,31 +1,19 @@
-// function dropDownsys() {
-//   let dropDowns = document.querySelectorAll(".dropdown");
-//   for (i = 0; i < dropDowns.length; i++) {
-//     calcItems = dropDowns[i].querySelectorAll(".calc-item__value");
-//     let calcItemsSum = 0;
-//     for (j = 0; j < calcItems.length; j++) {
-//       calcItemsSum += Number(calcItems[j].value);
-//     }
-//     let info = dropDowns[i].querySelector(".dropdown__info > input");
-//     info.value = calcItemsSum;
-//   }
-// }
-
 // склонение слов (1 гость, 2 гостя, 5 гостей)
 function declension(number, a1, b2, c5) {
   let text = "";
   let num1 = number % 10;
   let num2 = number % 100;
-  if (num2 >= 5 && num2 <= 20 || num1 >= 5 && num1 <= 9 || num1 == 0 && number != 0) {
-    return text = String(number) + c5;
-  }
-  else if (num1 >= 2 && num1 <= 4) {
-    return text = String(number) + b2;
-  }
-  else if (num1 == 1) {
-    return text = String(number) + a1;
-  }
-  else return text = "";
+  if (
+    (num2 >= 5 && num2 <= 20) ||
+    (num1 >= 5 && num1 <= 9) ||
+    (num1 == 0 && number != 0)
+  ) {
+    return (text = String(number) + c5);
+  } else if (num1 >= 2 && num1 <= 4) {
+    return (text = String(number) + b2);
+  } else if (num1 == 1) {
+    return (text = String(number) + a1);
+  } else return (text = "");
 }
 
 // заполняем шапку количеством гостей
@@ -47,37 +35,50 @@ function dropDownsays() {
     });
 
     let info = dropDown.querySelector(".dropdown__info > input");
-    let babiesNums = declension(babiesNum, " младенец", " младенца", " младенцев");
-    let guests = declension(adultsNum + childrenNum, " гость", " гостя", " гостей");
+    let babiesNums = declension(
+      babiesNum,
+      " младенец",
+      " младенца",
+      " младенцев"
+    );
+    let guests = declension(
+      adultsNum + childrenNum,
+      " гость",
+      " гостя",
+      " гостей"
+    );
 
     if (adultsNum + childrenNum > 0 && babiesNum > 0) {
       info.value = guests + ", " + babiesNums;
-    }
-    else if (adultsNum + childrenNum > 0 || babiesNum > 0) {
+    } else if (adultsNum + childrenNum > 0 || babiesNum > 0) {
       info.value = guests + babiesNums;
-    }
-    else info.value = "";
+    } else info.value = "";
 
-    let aClear = dropDown.querySelector('.calc-buttons_clear');
+    let aClear = dropDown.querySelector(".calc-buttons_clear");
     if (adultsNum + childrenNum + babiesNum > 0) {
-      aClear.classList.add('calc-buttons_clear-vis');
+      aClear.classList.add("calc-buttons_clear-vis");
     }
     if (adultsNum + childrenNum + babiesNum == 0) {
-      aClear.classList.remove('calc-buttons_clear-vis');
+      aClear.classList.remove("calc-buttons_clear-vis");
     }
-    // скрывать или показывать калькулятор
-    let calculator = dropDown.querySelector('.dropdown__calculator');
-    // if (calculator.getAttribute(data-show) == "show") {
-      // calculator.classList.add('show_calc');
-    // }
-    // else {
-      calculator.classList.remove('dropdown__calculator_show');
-    // }
-    
   });
 }
 
 document.addEventListener("DOMContentLoaded", dropDownsays());
+document.addEventListener("DOMContentLoaded", dropDownsaysi());
+
+function dropDownsaysi() {
+  let dropDowns = document.querySelectorAll(".dropdown");
+  dropDowns.forEach(function(dropDown) {
+    // свернуть или развернуть dropdown
+    if (dropDown.getAttribute("show") == 1) {
+      dropdownCalculator = dropDown.querySelector(".dropdown__calculator");
+      dropdownCalculator.classList.toggle("dropdown__calculator_show");
+      inf = dropDown.querySelector('.dropdown__info');
+      inf.classList.toggle("dropdown__info_border");
+    }
+  });
+}
 
 let calcClick = document.querySelectorAll(".calc-item__operator");
 for (i = 0; i < calcClick.length; i++) {
@@ -111,12 +112,13 @@ function frash() {
 }
 
 // кнопка скрыть/показать калькулятор
-let showCalc = document.querySelectorAll('.dropdown__info > button');
-showCalc.forEach((el) => el.addEventListener('click', funShowCalc));
+let showCalc = document.querySelectorAll(".dropdown__info > button");
+showCalc.forEach((el) => el.addEventListener("click", funShowCalc));
 
 function funShowCalc(e) {
-  console.log('hello!')
   let inf = this.parentElement;
+  inf.classList.toggle("dropdown__info_border");
+
   let calc = inf.nextElementSibling;
-  calc.classList.toggle('dropdown__calculator_show');
+  calc.classList.toggle("dropdown__calculator_show");
 }
